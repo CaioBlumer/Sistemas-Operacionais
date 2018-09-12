48186a36c99779a32c182345e3fde5581fe5d19e
CaioBlumer <caioblumer5@gmail.com>
2018-09-12 17:08:59 -0300

    Added files
    -- first commit with files --

---
+++ cliente.c
@@ -0,0 +1,12 @@
+/*TRABALHO SISTEMAS OPERACIONAIS*
+/*NOMES : ALECSANDER GESSER, CAIO BLUMER*/
+/**/
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(){
+
+  return 0;
+}
